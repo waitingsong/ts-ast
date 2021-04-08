d661574fac8b09b1b34de54b08424721cc5ac8c6
waiting <1661926154@qq.com>
2021-04-08 11:48:37 +0800

    test(types-dev): clean

--- packages/shared-types-dev/test/literal/demo5.ts.example.ts
+++
@@ -1,7 +0,0 @@
-import { transPlaceHolder } from './dict'
-import { Db, Db2, DbDict } from './types'
-
-
-export const dict1: DbDict<Db> = transPlaceHolder<Db>()
-export const dict2: DbDict<Db2> = transPlaceHolder<Db2>()
-
