6b660a653e9d6dfb0a397c8bc4082b75b0fa0dd1
waiting <1661926154@qq.com>
2021-05-27 21:55:21 +0800

    test: ignore

--- packages/shared-types-dev/test/transformer-type-to-literal-obj/demo6.ts
+++ packages/shared-types-dev/test/transformer-type-to-literal-obj/demo6.ts
@@ -1,26 +1,26 @@
-import { computeCallExpressionToLiteralObj } from '../../src'
-import { Db, DbDict } from '../literal/types'
+// import { computeCallExpressionToLiteralObj } from '../../src'
+// import { Db, DbDict } from '../literal/types'
 
 
-export function genDbDict<D>(): DbDict<D> {
-  const needle = 'genDbDict'
-  const ret = computeCallExpressionToLiteralObj(needle)
-  return ret as DbDict<D>
-}
+// export function genDbDict<D>(): DbDict<D> {
+//   const needle = 'genDbDict'
+//   const ret = computeCallExpressionToLiteralObj(needle)
+//   return ret as DbDict<D>
+// }
 
-export function alter<D>(): DbDict<D> {
-  const ret = computeCallExpressionToLiteralObj()
-  return ret as DbDict<D>
-}
+// export function alter<D>(): DbDict<D> {
+//   const ret = computeCallExpressionToLiteralObj()
+//   return ret as DbDict<D>
+// }
 
-export function fake<D>(): DbDict<D> {
-  const needle = 'genDbDict'
-  const ret = computeCallExpressionToLiteralObj(needle)
-  return ret as DbDict<D>
-}
+// export function fake<D>(): DbDict<D> {
+//   const needle = 'genDbDict'
+//   const ret = computeCallExpressionToLiteralObj(needle)
+//   return ret as DbDict<D>
+// }
 
-export {
-  Db,
-  DbDict,
-}
+// export {
+//   Db,
+//   DbDict,
+// }
 
