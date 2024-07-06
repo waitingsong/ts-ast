a7b8b3811771508adf76bba29237f745008e9eb7
waiting <1661926154@qq.com>
2024-07-06 23:54:19 +0800

    refactor: ts import

--- packages/shared-types-dev/src/lib/ts/ts-common.ts
+++ packages/shared-types-dev/src/lib/ts/ts-common.ts
@@ -3,13 +3,29 @@ import { dirname } from 'node:path'
 
 import { pathResolve } from '@waiting/shared-core'
 import type { LiteralObject } from '@waiting/shared-types'
-// eslint-disable-next-line import/no-extraneous-dependencies
-import ts from 'typescript'
+import {
+  type ImportClause,
+  type ImportDeclaration,
+  type ImportSpecifier,
+  type NamedImports,
+  type Node,
+  type PropertyAssignment,
+  type ObjectLiteralExpression,
+  type ObjectLiteralElementLike,
+  type StringLiteral,
+  type TypeChecker,
+  type CallExpression,
+  factory,
+  isCallExpression,
+  isImportDeclaration,
+  isJSDocSignature,
+  SyntaxKind,
+ } from 'typescript'
 
 
-export function createObjectLiteralExpression(input: LiteralObject): ts.ObjectLiteralExpression {
+export function createObjectLiteralExpression(input: LiteralObject): ObjectLiteralExpression {
 
-  const arr: ts.ObjectLiteralElementLike[] = Object.entries(input).map(([key, value]) => {
+  const arr: ObjectLiteralElementLike[] = Object.entries(input).map(([key, value]) => {
     if (Array.isArray(value)) {
       throw new TypeError('property value not literal object, but array. key: ' + key)
     }
@@ -23,18 +39,18 @@ export function createObjectLiteralExpression(input: LiteralObject): ts.ObjectLi
     }
     throw new TypeError('property value not literal object. key: ' + key)
   })
-  const ret = ts.factory.createObjectLiteralExpression(arr, true)
+  const ret = factory.createObjectLiteralExpression(arr, true)
   return ret
 }
 
 function createPropertyAssignmentOfString(
   key: string,
   value: string,
-): ts.PropertyAssignment {
+): PropertyAssignment {
 
-  const ret = ts.factory.createPropertyAssignment(
-    ts.factory.createIdentifier(key),
-    ts.factory.createStringLiteral(value),
+  const ret = factory.createPropertyAssignment(
+    factory.createIdentifier(key),
+    factory.createStringLiteral(value),
   )
   return ret
 }
@@ -43,12 +59,12 @@ function createPropertyAssignmentOfString(
 function createPropertyAssignmentOfObject(
   key: string,
   value: LiteralObject,
-): ts.PropertyAssignment {
+): PropertyAssignment {
 
-  const id = ts.factory.createIdentifier(key)
+  const id = factory.createIdentifier(key)
   const expression = createObjectLiteralExpression(value)
 
-  const arr = ts.factory.createPropertyAssignment(
+  const arr = factory.createPropertyAssignment(
     id,
     expression,
   )
@@ -61,11 +77,11 @@ function createPropertyAssignmentOfObject(
  * return undefined if none importSpecifier according to param skipSpecifiers
  */
 export function processImportDeclaration(
-  node: ts.ImportDeclaration,
+  node: ImportDeclaration,
   skipSpecifiers: string[],
-): ts.ImportDeclaration | undefined {
+): ImportDeclaration | undefined {
 
-  const module = (node.moduleSpecifier as ts.StringLiteral).text // not getText()
+  const module = (node.moduleSpecifier as StringLiteral).text // not getText()
   // console.info({ module })
   if (! module) {
     return
@@ -75,8 +91,8 @@ export function processImportDeclaration(
 
   // const binds = node.importClause?.namedBindings
   const binds = node.importClause ? node.importClause.namedBindings : void 0
-  if (binds && binds.kind === ts.SyntaxKind.NamedImports) {
-    binds.elements.forEach((elm: ts.ImportSpecifier) => {
+  if (binds && binds.kind === SyntaxKind.NamedImports) {
+    binds.elements.forEach((elm: ImportSpecifier) => {
       const elmText = elm.getText()
       if (elmText && ! skipSpecifiers.includes(elmText)) {
         st.add(elmText)
@@ -88,28 +104,28 @@ export function processImportDeclaration(
     return
   }
 
-  const arr: ts.ImportSpecifier[] = []
+  const arr: ImportSpecifier[] = []
   st.forEach((name) => {
-    const sp = ts.factory.createImportSpecifier(
+    const sp = factory.createImportSpecifier(
       true,
-      ts.factory.createIdentifier(name),
-      ts.factory.createIdentifier(name),
+      factory.createIdentifier(name),
+      factory.createIdentifier(name),
     )
     arr.push(sp)
   })
   if (! arr.length) {
     return
   }
-  const nameImports: ts.NamedImports = ts.factory.createNamedImports(arr)
-  const importClause: ts.ImportClause = ts.factory.createImportClause(
+  const nameImports: NamedImports = factory.createNamedImports(arr)
+  const importClause: ImportClause = factory.createImportClause(
     false,
     void 0,
     nameImports,
   )
-  const importDecl: ts.ImportDeclaration = ts.factory.createImportDeclaration(
+  const importDecl: ImportDeclaration = factory.createImportDeclaration(
     void 0,
     importClause,
-    ts.factory.createStringLiteral(module),
+    factory.createStringLiteral(module),
   )
   return importDecl
 }
@@ -119,12 +135,12 @@ export function processImportDeclaration(
  * Note: do NOT compare tsPath if both jsPath and tsPath are blank string and is ts.isImportDeclaration
  */
 export function isKeysImportExpression(
-  node: ts.Node,
+  node: Node,
   jsPath: string,
   tsPath: string,
-): node is ts.ImportDeclaration {
+): node is ImportDeclaration {
 
-  if (! ts.isImportDeclaration(node)) {
+  if (! isImportDeclaration(node)) {
     return false
   }
 
@@ -132,7 +148,7 @@ export function isKeysImportExpression(
     return true // !
   }
 
-  const module = (node.moduleSpecifier as ts.StringLiteral).text // not getText()
+  const module = (node.moduleSpecifier as StringLiteral).text // not getText()
   try {
     if (module.startsWith('.')) {
       const resolvedPath = pathResolve(dirname(node.getSourceFile().fileName), module)
@@ -161,13 +177,13 @@ export function isKeysImportExpression(
  * Note: do NOT compare tsPath if tsPath is blank string and is ts.isCallExpression
  */
 export function isKeysCallExpression(
-  node: ts.Node,
-  typeChecker: ts.TypeChecker,
+  node: Node,
+  typeChecker: TypeChecker,
   needleName: string,
   tsPath: string, // if blank string, return true
-): node is ts.CallExpression {
+): node is CallExpression {
 
-  if (! ts.isCallExpression(node)) {
+  if (! isCallExpression(node)) {
     return false
   }
   const sign = typeChecker.getResolvedSignature(node)
@@ -175,7 +191,7 @@ export function isKeysCallExpression(
     return false
   }
   const { declaration } = sign
-  if (! declaration || ts.isJSDocSignature(declaration)) {
+  if (! declaration || isJSDocSignature(declaration)) {
     return false
   }
   else {
